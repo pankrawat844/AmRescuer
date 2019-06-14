a07291d12aeb9b98baaeaea0fe1d9e6e95e98eb7
pankaj <pankawat9@gmail.com>
2019-06-14 15:07:25 +0530

    bottom app bar added

---
+++ app/src/main/java/com/app/amrescuer/Adapter/ViewPagerAdapter.java
@@ -0,0 +1,26 @@
+package com.app.amrescuer.Adapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+class ViewPagerAdaptere extends FragmentPagerAdapter {
+    List<String> list= new ArrayList<>();
+    public ViewPagerAdaptere(FragmentManager fm, List<String> list) {
+        super(fm);
+        this.list=list;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+}
