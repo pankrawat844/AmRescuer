026c1ae725fb25d08e84b692c386f4cb50022cd5
pankdev09 <pankawat9@gmail.com>
2019-09-12 17:39:20 +0530

    add dashboard recylerview

--- app/src/main/java/com/app/amrescuer/fragment/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/app/amrescuer/fragment/dashboard/DashboardFragment.kt
@@ -9,10 +9,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.app.amrescuer.R
+import com.app.amrescuer.room.entities.Feed
 import com.app.amrescuer.utils.Coroutines
+import com.app.amrescuer.utils.hide
+import com.app.amrescuer.utils.show
 import com.app.amrescuer.utils.toast
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.dashboard_fragment.*
 import org.kodein.di.android.x.kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.generic.instance
@@ -33,11 +40,32 @@ class DashboardFragment : Fragment(),KodeinAware {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this,factory).get(DashboardViewModel::class.java)
-        Coroutines.main {
-            val feeds=viewModel.feeds.await()
-            feeds.observe(this, Observer {
-                context?.toast(it.size.toString())
-            })
+       bindUI()
+    }
+
+    private fun bindUI() =Coroutines.main {
+        progress_bar.show()
+        viewModel.feeds.await().observe(this, Observer {
+            progress_bar.hide()
+            initRecylerview(it.toFeedItem()  )
+        })
+    }
+
+    private fun initRecylerview(toFeedItem: List<FeedItem>) {
+        val adapt=GroupAdapter<ViewHolder>().apply {
+            addAll(toFeedItem)
+        }
+        recyclerview.apply {
+            layoutManager=LinearLayoutManager(context)
+            setHasFixedSize(true)
+            adapter=adapt
+
+        }
+    }
+
+    private fun List<Feed>.toFeedItem():List<FeedItem>{
+        return this.map {
+            FeedItem(it)
         }
     }
 
---
+++ app/src/main/java/com/app/amrescuer/fragment/dashboard/FeedItem.kt
@@ -0,0 +1,19 @@
+package com.app.amrescuer.fragment.dashboard
+
+import com.app.amrescuer.R
+import com.app.amrescuer.databinding.ItemDashboardBinding
+import com.app.amrescuer.databinding.ItemFeedBindingImpl
+import com.app.amrescuer.room.entities.Feed
+import com.xwray.groupie.databinding.BindableItem
+
+class FeedItem(
+        private val feed:Feed
+):BindableItem<ItemDashboardBinding>() {
+    override fun getLayout(): Int {
+        return R.layout.item_dashboard
+    }
+
+    override fun bind(viewBinding: ItemDashboardBinding, position: Int) {
+       viewBinding.feed=feed
+    }
+}
